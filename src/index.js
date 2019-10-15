67136c6bace74325aa2a9de69e73f2e9b6fd5dbc
Austin Hedeen <austin.hedeen@gmail.com>
2019-10-15 15:35:12 -0700

    Contain routing logic in App component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import createStore from './store/create-store';
 
 const store = createStore();
 
-
 render(
   <Provider store={store}>
-   <BrowserRouter>
-      <App />
-   </BrowserRouter>
+    <App />
   </Provider>,
   document.querySelector('#root'),
 );
