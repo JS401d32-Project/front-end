8cdc31ccf34dff23e7fdc2e6d916e99b70e9eba9
hannaalemu <hanna9alemu@outlook.com>
2019-10-17 09:24:14 -0700

    pulling in development

--- src/components/contact/contacts-form.js
+++ src/components/contact/contacts-form.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
-import './contact.scss'
+import './contact.scss';
 
 import { connect } from 'react-redux';
 
