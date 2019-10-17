8c584ae683148cd707e8a9965eeaa006e658ca75
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-10-17 09:36:49 -0700

    ui-frontEnd

--- src/components/case/case.js
+++ src/components/case/case.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './case.scss'
 
 import CaseForm from './case-form/case-form';
 import ClientContact from '../contact/contact-render';
@@ -33,6 +34,7 @@ function Case(props) {
 
   return (
     <>
+      <div className='caseContainer'>
       {ready  
         ? <>
           <CaseForm /> 
@@ -70,6 +72,7 @@ function Case(props) {
           {/* <AssociatedContacts /> */}
         </>
         : null}
+      </div>
     </>
   );
 }
